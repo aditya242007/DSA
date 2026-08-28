04f7e05ffff4b76c3cd3be35d6b554df896054c9
Aditya Kalure <adityakalure237@gmail.com>
2026-08-28 13:09:26 +0000

    Sort any array using RecursionSort

---
+++ RECURSION/sort_array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Insert(vector<int>& arr,int last){
+    if(arr.size() == 0 || arr.back() <= last){
+        arr.push_back(last);
+        return ;
+    }
+
+    int val = arr.back();
+    arr.pop_back();
+
+    Insert(arr,last);
+
+    arr.push_back(val);           // push back the last element
+}
+
+void Sort(vector<int>& arr){
+    if(arr.size() == 1) return ;
+
+    int last = arr.back();
+    arr.pop_back();
+
+    Sort(arr);                    // recursive call
+
+    Insert(arr,last);             // insert last element in sorted array
+
+}
+
+int main(){
+    vector<int> arr = {5,1,1,2,0,0};
+
+    Sort(arr);                    // main function
+
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
